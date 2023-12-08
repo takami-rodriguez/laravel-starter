a84d8a1bdb58b8f4b10a0d53b53f3a777fae1da4
Takami <takami.rodriguez@gmail.com>
2023-12-08 12:13:08 -0600

    Fixed permission and variable for comments

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,6 +19,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if (env('APP_ENV') === 'stage') {
+            \URL::forceScheme('https');
+        }
         Schema::defaultStringLength(191);
     }
 }
